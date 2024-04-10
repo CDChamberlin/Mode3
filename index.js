4d961b4ffa121c418f6ab1b294369e6c1ed659eb
Collin <cdchamberlin.rva@gmail.com>
2024-04-10 19:55:52 -0400

    Added a second function

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function myFunction(num1, string1) {
     console.log(`Hello from myFunction, ${string1}.`);
     for (i = 0; i < num1; i += 2) console.log(i);
 }
+
+function function2() {
+    console.log("This is a function");
+}
